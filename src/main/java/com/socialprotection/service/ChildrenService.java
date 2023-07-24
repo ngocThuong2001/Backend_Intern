f82c68ff78db359ad25faa0cdbe9aec6cb19227c
Nbtrien <trienbanguyen@gmail.com>
2023-07-24 21:14:02 +0700

    m

--- src/main/java/com/socialprotection/service/ChildrenService.java
+++ src/main/java/com/socialprotection/service/ChildrenService.java
@@ -1,5 +1,11 @@
 package com.socialprotection.service;
 
-public interface ChildrenService {
+import com.socialprotection.entity.Children;
+import com.socialprotection.entity.ChildrenStatus;
+import com.socialprotection.entity.TypeOfOrphan;
 
+public interface ChildrenService {
+	public Children save(Children children);
+	public TypeOfOrphan saveTypeOrphan(TypeOfOrphan typeOfOrphan);
+	public ChildrenStatus saveChildrenStatus(ChildrenStatus childrenStatus);
 }
--- src/main/java/com/socialprotection/service/impl/ChildrenServiceImpl.java
+++ src/main/java/com/socialprotection/service/impl/ChildrenServiceImpl.java
@@ -3,6 +3,9 @@ package com.socialprotection.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.socialprotection.entity.Children;
+import com.socialprotection.entity.ChildrenStatus;
+import com.socialprotection.entity.TypeOfOrphan;
 import com.socialprotection.repository.ChildrenRepository;
 import com.socialprotection.repository.ChildrenStatusRepository;
 import com.socialprotection.repository.TypeOfOrphanRepopsitory;
@@ -18,4 +21,19 @@ public class ChildrenServiceImpl implements ChildrenService{
 	
 	@Autowired 
 	private TypeOfOrphanRepopsitory typeOfOrphanRepopsitory;
+
+	@Override
+	public Children save(Children children) {
+		return childrenRepository.save(children);
+	}
+
+	@Override
+	public TypeOfOrphan saveTypeOrphan(TypeOfOrphan typeOfOrphan) {
+		return typeOfOrphanRepopsitory.save(typeOfOrphan);
+	}
+
+	@Override
+	public ChildrenStatus saveChildrenStatus(ChildrenStatus childrenStatus) {
+		return childrenStatusRepository.save(childrenStatus);
+	}
 }
